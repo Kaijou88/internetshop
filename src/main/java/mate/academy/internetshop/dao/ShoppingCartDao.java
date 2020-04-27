6c78147d96be2cac43dad779a4bef573b5ed27b2
Maryna <kaijou88@gmail.com>
2020-04-27 13:34:24 +0300

    made refactoring of DAO and Service layers, added IndexController with webapp

--- src/main/java/mate/academy/internetshop/dao/ShoppingCartDao.java
+++ src/main/java/mate/academy/internetshop/dao/ShoppingCartDao.java
@@ -1,17 +1,6 @@
 package mate.academy.internetshop.dao;
 
-import java.util.List;
-import java.util.Optional;
 import mate.academy.internetshop.model.ShoppingCart;
 
-public interface ShoppingCartDao {
-    ShoppingCart create(ShoppingCart shoppingCart);
-
-    boolean delete(Long id);
-
-    ShoppingCart update(ShoppingCart shoppingCart);
-
-    List<ShoppingCart> getAll();
-
-    Optional<ShoppingCart> get(Long id);
+public interface ShoppingCartDao extends GenericDao<ShoppingCart, Long> {
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mfran
+  Date: 4/27/2020
+  Time: 12:54 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello world!</h1>
+</body>
+</html>
